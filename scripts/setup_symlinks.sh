ae16ae648bf3ba899e7e8742bb9d8afef40f5796
xo-xo-xo-xo <93801988+xo-xo-xo-xo@users.noreply.github.com>
2025-09-28 22:05:58 -0700

    update

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# Sets up the rofi config directory and symlinks the rasi config files from the generated wal rasi files in ~/.cache/wal/
+# wal symlinks
 
-config_dir="${HOME}/.config/rofi"
+# directories to create
+for dir in \
+  "$HOME/.config/rofi" \
+  "$HOME/.local/share/TauonMusicBox/theme" \
+  "$HOME/.config/heroic/heroic-themes-main/pywal" \
+  "$HOME/.config/gtk-3.0" \
+  "$HOME/.config/gtk-4.0"; do
+    mkdir -p "$dir"
+done
 
-if [ ! -d "${config_dir}" ] ; then
-	mkdir -p "${config_dir}"
-fi
-
-ln -s ~/.cache/wal/rofi-colors.rasi ~/.config/rofi/colors.rasi
-ln -s ~/.cache/wal/rofi-simple-config.rasi ~/.config/rofi/simple-config.rasi
-ln -s ~/.cache/wal/rofi-powermenu-config.rasi ~/.config/rofi/powermenu-config.rasi
-ln -s ~/.cache/wal/rofi-launcher-config.rasi ~/.config/rofi/launcher-config.rasi
-ln -s ~/.cache/wal/rofi-wallpaper-sel.rasi ~/.config/rofi/wallpaper-sel-config.rasi
+# Symlinks (source -> target)
+ln -sf ~/.cache/wal/rofi-colors.rasi       ~/.config/rofi/colors.rasi
+ln -sf ~/.cache/wal/rofi-simple-config.rasi ~/.config/rofi/simple-config.rasi
+ln -sf ~/.cache/wal/rofi-powermenu-config.rasi ~/.config/rofi/powermenu-config.rasi
+ln -sf ~/.cache/wal/rofi-launcher-config.rasi ~/.config/rofi/launcher-config.rasi
+ln -sf ~/.cache/wal/rofi-wallpaper-sel.rasi  ~/.config/rofi/wallpaper-sel-config.rasi
+ln -sf ~/.cache/wal/colors-tauon.ttheme      ~/.local/share/TauonMusicBox/theme/colors-tauon.ttheme
+ln -sf ~/.cache/wal/heroic-pywal.css        ~/.config/heroic/heroic-themes-main/pywal/pywal.css
+ln -sf ~/.cache/wal/gtk-colors.css          ~/.config/gtk-3.0/colors.css
+ln -sf ~/.cache/wal/gtk-colors.css          ~/.config/gtk-4.0/colors.css
